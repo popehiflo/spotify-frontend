caa5ebfdefd9de754143889aa23805e094b2c500
Pool Petter Hijuela Florian <poolpetterhijuel@hotmail.com>
2022-01-04 16:50:39 -0500

    crea componente compartido MediaPlayer

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { MediaPlayerComponent } from './components/media-player/media-player.component';
 
 
 
 @NgModule({
   declarations: [
-    SideBarComponent
+    SideBarComponent,
+    MediaPlayerComponent
   ],
   imports: [
     CommonModule
