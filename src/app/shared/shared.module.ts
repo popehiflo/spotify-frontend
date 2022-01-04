d452c78be713d46c7696c01b2065614d6fcc2761
Pool Petter Hijuela Florian <poolpetterhijuel@hotmail.com>
2022-01-04 16:48:48 -0500

    crea componente compartido SideBar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SideBarComponent
+  ],
   imports: [
     CommonModule
   ]
